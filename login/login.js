b313f723caa4b19c05500f5cc7d5805cf46d279f
adamosnacho <adamoscom98@gmail.com>
2025-03-17 20:36:44 +0100

    Fixed register

--- login/login.js
+++ login/login.js
@@ -1,11 +1,23 @@
 function createWallet() {
     setCookie("password", ge("passwd").value);
+    const encryptedWallet = CryptoJS.AES.encrypt(
+        JSON.stringify({}),
+        getCookie("password")
+    ).toString();
 
-    fetch(apiServer + "/register?wallet=" + CryptoJS.AES.encrypt("{}", ge("passwd").value).toString()).then(res => res.json()).then(data => {
-        alert("This is your wallet token: " + data.token + " Write it down somewhere safe, you will need it to log in the next time!");
+    fetch(apiServer + "/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            wallet: encryptedWallet
+        }),
+    }).then(response => response.json()).then(data => {
+        alert("This is your wallet token: " + data.token + " Write it down somewhere safe and DO NOT SHARE IT, you will need it to log in the next time! (You can take a screenshot)");
         setCookie("token", data.token);
         document.location.href = "/wallet";
-    });
+    }).catch(error => console.error("Error creating wallet:", error));
 }
 
 function loginToWallet() {
