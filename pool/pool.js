c379a2db391abf5b66cb93584158014dc65376e1
adamosnacho <adamoscom98@gmail.com>
2025-04-21 09:57:49 +0200

    Fixed pool payout

--- pool/pool.js
+++ pool/pool.js
@@ -15,13 +15,31 @@ function checkRewards(event) {
 
 function payout(event) {
     event.preventDefault();
+    const form = document.getElementById('poForm');
+    const elements = form.elements;
+
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].disabled = true;
+    }
+
     const ps = document.getElementById("poolSecret").value;
     if (ps === "") {
         document.getElementById("error").innerText = "please provide your pool secret";
         return;
     }
     fetch(poolServer + "/payout/" + ps).then(res => res.json()).then(data => {
-        if (data.error) document.getElementById("error").innerText = data.error;
+        if (data.error) {
+            if (data.error === "Already in the paying out process.") {
+                fetch(poolServer + "/process/" + ps).then(res => res.json()).then(data => {
+                    if (!data.error) {
+                        document.getElementById("transid").innerText = "(Recovered) Transaction ID: " + data.id;
+                        document.getElementById("finish").style.display = "";
+                    }
+                });
+                return
+            }
+            document.getElementById("error").innerText = data.error;
+        }
         if (!data.error || data.error !== "Already paid out your rewards, or you have not yet mined any.") document.getElementById("finish").style.display = "";
         if (!data.error) document.getElementById("transid").innerText = "Transaction ID: " + data.id;
     });
